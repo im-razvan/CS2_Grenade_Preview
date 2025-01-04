8f3d7019a852fc49c69a1d11265182b57ee9e72f
Iacob Razvan <99391840+im-razvan@users.noreply.github.com>
2025-01-04 21:50:30 +0200

    Update main.py
    update lol i needed this

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 #sv_grenade_trajectory_prac_pipreview
-from pymem import Pymem, process
-from re import search
+from pymem import *
 
-patch = b'\x0f\x85'
-location = rb'\x0f\x84....\x8b\x05....\x48\x89\x74\x24.\xbe' 
-# 0f 84 ? ? ? ? 8b 05 ? ? ? ? 48 89 74 24 ? be
+patch = b'\x0F\x85'
+location = rb'\x0F\x84....\x8B\x05....\x48\x89\x9C\x24....\xBB' 
+# 0F 84 ? ? ? ? 8B 05 ? ? ? ? 48 89 9C 24 ? ? ? ? BB
 
 pm = Pymem('cs2.exe')
+client = pymem.process.module_from_name(pm.process_handle, 'client.dll')
 
-client = process.module_from_name(pm.process_handle, 'client.dll')
-clientBytes = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage) 
+address = pymem.pattern.pattern_scan_module(pm.process_handle, client, location)
+if not address: raise Exception("Signature not found.")
 
-address = client.lpBaseOfDll + search(location, clientBytes).start()
+print("[+] found signature @ %s" % hex(address))
+
 pm.write_bytes(address, patch, len(patch))
+
+print("[+] patched bytes")
+
+"""
+WARNING: Byte patching should be detected by VAC.
+Don't use it on a good account.
+
+This script swaps the grenade preview behavior,
+displaying it when sv_grenade_trajectory_prac_pipreview is set to 0 instead of 1.
+"""
